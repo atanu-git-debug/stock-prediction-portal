afe66eff21ea307454d3a09f7e82d756625d3de9
atanu-git-dubug <imatanu0@gmail.com>
2026-03-05 09:58:18 +0530

    fixed the error

--- frontend-REACT/src/PrivateRoute.jsx
+++ frontend-REACT/src/PrivateRoute.jsx
@@ -1,11 +1,11 @@
-import {Children, useContext} from 'react'
+import {useContext} from 'react'
 import { AuthContext } from './AuthProvider'
 import { Navigate } from 'react-router-dom'
 
 
-const PrivateRoute = (children) => {
-    const {isLoggedIn} = useContext(AuthContext)
-  return isLoggedIn ?(
+const PrivateRoute = ({children}) => {
+    const {isloggedin} = useContext(AuthContext)
+  return isloggedin ?(
         children
   ):(
     <Navigate to='/login' />
