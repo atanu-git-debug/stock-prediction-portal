8de71cf5227f7f270fbc075635b410106d0fda64
atanu-git-dubug <imatanu0@gmail.com>
2026-03-14 19:34:07 +0530

    added requirements.txt file

--- frontend-REACT/src/components/Main.jsx
+++ frontend-REACT/src/components/Main.jsx
@@ -9,7 +9,11 @@ const Main = () => {
         <div className='container'>
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
-                <p className='text-light lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ex porro similique facilis. Quisquam est modi eaque blanditiis doloremque suscipit accusantium, voluptatem quibusdam facilis nostrum nesciunt, necessitatibus illo commodi praesentium?</p>
+                <p className='text-light lead'>AI-Driven Stock Prediction Platform. <br />
+
+Explore stock market trends using machine learning. This platform analyzes historical data and uses deep learning models to forecast possible future price movements, providing insights through interactive charts and analytics.
+
+Start exploring smarter market predictions today.</p>
                 <div>
                     <Button text='Explore Now' url="/dashboard" className='btn-info' />
                 </div>
